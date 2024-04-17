4833558436d9effb8da5b6fbb8c026963e3a6755
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-04-18 01:55:21 +0200

    updated

--- auth.js
+++ auth.js
@@ -1,30 +1,35 @@
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const jwtSecret = process.env.JWT_SECRET || "MySecretKey2024!";
+const jwtSecret ='MySecretKey2024!';
 
-require("./passport"); // Your local passport file
+const jwt = require('jsonwebtoken'),
+passport = require('passport');
+
+require('./passport');
 
 let generateJWTToken = (user) => {
-  return jwt.sign({ username: user.username }, jwtSecret, {
-      expiresIn: "7d",
-  });
-};
+    return jwt.sign(user, jwtSecret, {
+        subject: user.UserName,
+        expiresIn: '7d',
+        algorithm: 'HS256'
+    });
+}
 
-module.exports = (app) => {
-  app.post("/login", (req, res, next) => {
-      passport.authenticate("local", { session: false }, (error, user, info) => {
-          if (error || !user) {
-              return res.status(400).json({
-                  message: "Invalid username or password",
-              });
-          }
-          req.login(user, { session: false }, (error) => {
-              if (error) {
-                  res.send(error);
-              }
-              let token = generateJWTToken(user.toJSON());
-              return res.json({ user, token });
-          });
-      })(req, res, next);
-  });
-};
+//POST login.
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false}, (error, user, info) => {
+            if (error || !user){
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) =>{
+                if (error) {
+                    res.send(error);
+                }
+            let token = generateJWTToken(user.toJSON());
+            return res.json({user, token});
+            });
+        })(req, res);
+    });
+}
